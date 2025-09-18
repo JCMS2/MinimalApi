c1828a2cf0d25152844e9dfafe983e566cf61041
Jose Carlos <jc270922@gmail.com>
2025-09-18 17:32:43 -0300

    Primeiro commit do meu código local

---
+++ MinimalsApi/MinimalsApi/Infraestrutura/Db/DbContexto.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MinimalsApi.Dominio.Entidades;
+
+namespace MinimalsApi.Infraestrutura.Db
+{
+    public class DbContexto : DbContext
+    {
+        private readonly IConfiguration? _configuracaoAppSettings;
+
+        // Construtor para runtime (com configuração)
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options)
+        {
+        }
+
+        // Opcional, se quiser usar configuração direta no OnConfiguring (não obrigatório se usar o construtor acima)
+        public DbContexto(IConfiguration configuracaoAppSettings)
+        {
+            _configuracaoAppSettings = configuracaoAppSettings;
+        }
+
+        public DbContexto()
+        {
+        }
+
+        public DbSet<Adminstrador> Administradores { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Adminstrador>().HasData(
+                new Adminstrador 
+                {
+                      Email= "adm@teste.com",
+                      Senha= "123456",
+                      Perfil= "Adm"
+                }
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                if (_configuracaoAppSettings != null)
+                {
+                    var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql");
+                    if (!string.IsNullOrEmpty(stringConexao))
+                    {
+                        optionsBuilder.UseMySql(stringConexao, ServerVersion.AutoDetect(stringConexao));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MinimalsApi/MinimalsApi/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalsApi.Dominio.DTOs;
+using MinimalsApi.Infraestrutura.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello world");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
